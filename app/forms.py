b41824d252efa781fcaf69e597231538b62d6539
andreit <unfederal@list.ru>
2024-09-13 15:01:44 +0500

    new flask_news

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, SelectField, SubmitField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, Length, Optional, Email, EqualTo
 
 from .models import Category
 from . import app
@@ -19,4 +19,27 @@ class NewsForm(FlaskForm):
     text = TextAreaField('Текст',
                          validators=[DataRequired(message="Поле не должно быть пустым!")])
     category = SelectField(choices=get_categories())
+    submit = SubmitField('Добавить')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Некорректный email')])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Пароль', validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class CategoryForm(FlaskForm):
+    title = StringField('Название',
+                       validators=[DataRequired(message="Поле не должно быть пустым!"),
+                                   Length(max=20, message='Введите заголовок длиной до 20 символов')])
     submit = SubmitField('Добавить')
